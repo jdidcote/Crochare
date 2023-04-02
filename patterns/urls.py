da17ae117a2fa00326e0f9eea8a2d79e4ec8e06b
joshdidcote <joshua.didcote@outlook.com>
2023-04-02 23:11:51 +0100

    Add view pattern page

--- patterns/urls.py
+++ patterns/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('create-pattern', views.create_pattern, name='create-pattern'),
     path('edit-pattern/<int:pattern_id>', views.edit_pattern, name='edit-pattern'),
     path('my-patterns', views.get_user_patterns, name='user-patterns'),
-    path('my-patterns/delete/<int:pattern_id>', views.delete_pattern, name='delete-pattern')
+    path('my-patterns/delete/<int:pattern_id>', views.delete_pattern, name='delete-pattern'),
+    path('<int:pattern_id>', views.view_pattern, name='view-pattern'),
 ]
